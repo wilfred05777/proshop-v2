be1b7badc6100324cc4449365fa447d701cecc1d
Wilfred <wilfred05777@gmail.com>
2023-05-26 17:09:08 +0800

    Header & Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to ProShop</h1>
-    </div>
+    <>
+      <Header />
+      <main children='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
--- readme.md
+++ readme.md
@@ -1,5 +1,16 @@
 # ProShop V2
 
+<hr>
+
+- cd frontend
+- npm install react-bootstrap bootstrap react-icons
+
+- https://www.traversymedia.com/products/mern-stack-from-scratch-ecommerce-platform/categories/2152847614/posts/2167599607
+
+<hr>
+
 - cd frontend/
 - @cli @vscode: ls -a ( show all including hidden files)
 - rm -rf .git
+
+<hr>
