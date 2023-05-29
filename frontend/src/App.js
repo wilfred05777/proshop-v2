8d84d921ab37777c4fc18cc67fea5579094d7cd7
Wilfred <wilfred05777@gmail.com>
2023-05-30 04:55:43 +0800

    List product - initial bug on HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
       <Header />
-      <main children='py-3'>
+      <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          {/* <h1>Welcome to ProShop</h1> */}
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
